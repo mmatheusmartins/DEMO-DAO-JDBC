e52f97aa6a66dbf36d7453cb0f7010f02149456c
mmatheusmartins <mmatheusmartins03@gmail.com>
2023-11-13 16:28:24 -0300

    criação de projeto e repositório do git

---
+++ src/db/DB.java
@@ -0,0 +1,72 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+
+	private static Connection conn = null;
+
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties pr = loadProperties();
+				String url = pr.getProperty("dburl");
+				conn = DriverManager.getConnection(url, pr);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties pr = new Properties();
+			pr.load(fs);
+			return pr;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+
+	}
+
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+
+	}
+
+}
